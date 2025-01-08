f145e714e5aef1265b249d10eb98c046c930bff8
Harsh Sharma <harsh@codelif.in>
2025-01-09 05:25:53 +0530

    fix: sw caching fetch requests, stopping updates

--- public/sw.js
+++ public/sw.js
@@ -17,31 +17,31 @@ self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(
-                    (response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                cache.put(event.request, responseToCache);
-                            });
-                        return response;
-                    }
-                ).catch((error) => {
-                    console.error('Failed to fetch and cache:', error);
-                });
-            })
-    );
-});
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then((response) => {
+//                 if (response) {
+//                     return response;
+//                 }
+//                 return fetch(event.request).then(
+//                     (response) => {
+//                         if (!response || response.status !== 200 || response.type !== 'basic') {
+//                             return response;
+//                         }
+//                         const responseToCache = response.clone();
+//                         caches.open(CACHE_NAME)
+//                             .then((cache) => {
+//                                 cache.put(event.request, responseToCache);
+//                             });
+//                         return response;
+//                     }
+//                 ).catch((error) => {
+//                     console.error('Failed to fetch and cache:', error);
+//                 });
+//             })
+//     );
+// });
 
 self.addEventListener('push', function (event) {
     if (event.data) {
