456e4d777f6fdf0bfee1379edc9ac5a65573001a
Harsh Sharma <harsh@codelif.in>
2025-01-23 02:36:59 +0530

    chore: update sw cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jiit-planner-cache-v2025-01-22_19-58-07';
+const CACHE_NAME = 'jiit-planner-cache-v2025-01-23_02-36-47';
 const urlsToCache = [
     '/',
     // Add other assets you want to cache
