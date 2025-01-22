a00383e02a640f8e25a1c2ac0bbc404671fdfe96
Harsh Sharma <harsh@codelif.in>
2025-01-23 02:37:56 +0530

    feat: script updates

--- update-sw-timestamp.sh
+++ update-sw-timestamp.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 ROOTDIR=$(dirname "$(readlink -f "$0")")
 sed -i "s/.*jiit-planner-cache.*/$(date +"const CACHE_NAME = 'jiit-planner-cache-v%Y-%m-%d_%H-%M-%S';")/" "$ROOTDIR/public/sw.js"
+git add .
+git commit -m "chore: update sw cache"
