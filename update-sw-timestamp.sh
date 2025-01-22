3854c0d26fc8b94f5fa2518ff71403a78cc2b4ac
Harsh Sharma <harsh@codelif.in>
2025-01-22 20:10:39 +0530

    feat: update-sw-timestamp now works from any directory

--- update-sw-timestamp.sh
+++ update-sw-timestamp.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-sed -i "s/.*jiit-planner-cache.*/$(date +"const CACHE_NAME = 'jiit-planner-cache-v%Y-%m-%d_%H-%M-%S';")/" ./public/sw.js
+ROOTDIR=$(dirname "$(readlink -f "$0")")
+sed -i "s/.*jiit-planner-cache.*/$(date +"const CACHE_NAME = 'jiit-planner-cache-v%Y-%m-%d_%H-%M-%S';")/" "$ROOTDIR/public/sw.js"
